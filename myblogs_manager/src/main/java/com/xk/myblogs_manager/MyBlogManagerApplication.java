b6ac3c1baea3d94cba0e96140bc5d45c1740d3a3
SayShine <52228449+SayShine@users.noreply.github.com>
2020-01-09 11:19:53 +0800

    提交搭建好的项目

---
+++ myblogs_manager/src/main/java/com/xk/myblogs_manager/MyBlogManagerApplication.java
@@ -0,0 +1,13 @@
+package com.xk.myblogs_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBlogManagerApplication.class, args);
+    }
+
+}
